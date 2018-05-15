5e838fe69d680cd72dd916085d0aa3865ca648f7
hevlitt <hoedvaldezlo@ittepic.edu.mx>
2018-05-15 14:34:53 -0600

    Initial commit

---
+++ app/src/main/java/com/example/desktopn/a5_3sensoresgiroscopio/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.desktopn.a5_3sensoresgiroscopio;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private Sensor mysensor;
+    private SensorManager senman;
+    private TextView ejex,ejey,ejez;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        senman = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mysensor=senman.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        ejex = (TextView) findViewById(R.id.ejex);
+        ejey = (TextView) findViewById(R.id.ejey);
+        ejez = (TextView) findViewById(R.id.ejez);
+
+        senman.registerListener(this,mysensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        ejex.setText("Eje X:"+event.values[0]);
+        ejey.setText("Eje Y:"+event.values[1]);
+        ejez.setText("Eje Z:"+event.values[2]);
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
